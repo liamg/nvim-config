57debfb954983a687d2aee6ffc046e7e3f07f80d
Liam Galvin <liam.galvin@ghost.security>
2023-03-07 20:21:09 +0000

    misc

--- lua/config/keymaps/init.lua
+++ lua/config/keymaps/init.lua
@@ -31,7 +31,7 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
-keymap("n", "<Tab>", ":bnext<CR>", opts)
+--keymap("n", "<Tab>", ":bnext<CR>", opts)
 
 -- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
